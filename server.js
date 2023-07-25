f3036d9b1b6e56efd896a1c1d7381cda81ec434c
Damjan Savkovic <damjans92@gmail.com>
2023-07-25 15:28:35 +0200

    removed lines in server.js

--- server.js
+++ server.js
@@ -16,19 +16,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
-}
-
 app.use('/api/v1/transactions', transactions)
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/dist'))
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
-  )
-}
 
 const PORT = process.env.PORT || 5000
 
